6464321c5e5b00caf414273517e0d815f329958b
CaroBima <82395515+CaroBima@users.noreply.github.com>
2021-07-07 22:12:02 -0300

    Update java if-else.java

--- Java/java if-else.java
+++ Java/java if-else.java
@@ -1,15 +1,27 @@
+import java.io.*;
 import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
-
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner ene = new Scanner(System.in);
+        int n = ene.nextInt();
+        
+        int modulo = n%2; 
+        if (modulo!=0){
+            System.out.println("Weird");
+        } else {
+            if (n>=2 && n<=5){
+                System.out.println("Not Weird");
+            }else{
+                if(n>=6 && n<=20){
+                    System.out.println("Weird");
+                }else{
+                    System.out.println("Not Weird");
+                }
+            }
+            
+        }
     }
 }
